99eadbaa3d16328f94ef8710bf3a31c12acd4a92
Wan-Chen Chen <enlen111408@gmail.com>
2021-06-09 22:38:54 +0800

    Create predict.sh

---
+++ predict.sh
@@ -0,0 +1,126 @@
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_SGD_model1.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_SGD_model2.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_SGD_model3.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_SGD_model4.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_Adam_model5.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_Adam_model6.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_Adam_model7.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_Adam_model8.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_NewtonCG_model9.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_NewtonCG_model10.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_NewtonCG_model11.ckpt\
+		             --dim 28 28 1
+
+python3 predict.py --test_set ./data/mnist.t.mat \
+		             --model ./saved_model/MNIST_NewtonCG_model12.ckpt\
+		             --dim 28 28 1
+
+
+
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model_1.ckpt
+                 
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model_2.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model_3.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model_4.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model_5.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model_6.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model_7.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model_8.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model_9.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model_10.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model_11.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model_12.ckpt
+
+
+
+
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model1.ckpt
+                 
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model2.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model3.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model4.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model5.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model6.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model7.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model8.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model9.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model10.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model11.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model12.ckpt
