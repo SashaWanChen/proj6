d340efe4a8be2b68b6bab0c7be1d9acc6f0d8804
Wan-Chen Chen <enlen111408@gmail.com>
2021-06-09 15:14:46 +0800

    Create vgg7.sh

---
+++ vgg7.sh
@@ -0,0 +1,9 @@
+python3 train.py --optim Adam\
+		             --lr 0.0001 --C 0.1 \
+		             --net VGG11 --bsize 512 \
+		             --train_set ./data/cifar10-5000.mat \
+		             --model ./saved_model/CIFAR10_Adam_model7.ckpt \
+		             --log ./running_log/CIFAR10_Adam_VGG_logger3.log\
+		             --seed 85
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model7.ckpt
