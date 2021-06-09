8aeb8fd3ce4f60563d7e89f48195a451e27c98b4
Wan-Chen Chen <enlen111408@gmail.com>
2021-06-09 15:17:55 +0800

    Create vgg11.sh

---
+++ vgg11.sh
@@ -0,0 +1,10 @@
+python3 train.py --optim NewtonCG\
+		             --C 0.1 -lambda 0\
+		             --net VGG11 --bsize 3000 \
+		             --GNsize 1000 --CGmax  80 \
+		             --train_set ./data/cifar10-5000.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model11.ckpt \
+		             --log ./running_log/CIFAR10_NewtonCG_VGG_logger3.log\
+		             --seed 85
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model11.ckpt
