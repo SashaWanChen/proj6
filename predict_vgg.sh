70593f84ae0fdc5d1915d514ea6cb654b1f71b64
Wan-Chen Chen <enlen111408@gmail.com>
2021-06-11 15:01:51 +0800

    Create predict_vgg.sh

---
+++ predict_vgg.sh
@@ -0,0 +1,35 @@
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model1.ckpt
+                 
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model2.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model3.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_SGD_model4.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model5.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model6.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model7.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_Adam_model8.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model9.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model10.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model11.ckpt
+
+python3 predict.py --test_set ./data/cifar10.t.mat \
+		             --model ./saved_model/CIFAR10_NewtonCG_model12.ckpt
