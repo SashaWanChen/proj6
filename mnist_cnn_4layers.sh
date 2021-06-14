407a128a75b733b6a7d8ff3e4c72feb4e87d6459
Wan-Chen Chen <enlen111408@gmail.com>
2021-06-14 19:41:59 +0800

    Update mnist_cnn_4layers.sh

--- mnist_cnn_4layers.sh
+++ mnist_cnn_4layers.sh
@@ -3,6 +3,7 @@ python3 train.py --optim SGD\
 		             --net CNN_4layers --bsize 512 \
 		             --momentum 0.9 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_SGD_model1.ckpt\
 		             --log ./running_log/MNIST_SGD_logger1.log\
 		             --dim 28 28 1\
@@ -16,6 +17,7 @@ python3 train.py --optim SGD\
 		             --net CNN_4layers --bsize 512 \
 		             --momentum 0.9 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_SGD_model2.ckpt\
 			     --log ./running_log/MNIST_SGD_logger.log\
 		             --dim 28 28 1\
@@ -29,6 +31,7 @@ python3 train.py --optim SGD\
 		             --momentum 0.9 \
 		             --net CNN_4layers --bsize 512 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_SGD_model3.ckpt\
 		             --log ./running_log/MNIST_SGD_logger3.log\
 		             --dim 28 28 1\
@@ -42,6 +45,7 @@ python3 train.py --optim SGD\
 		             --momentum 0.9 \
 		             --net CNN_4layers --bsize 512 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_SGD_model4.ckpt\
 		             --log ./running_log/MNIST_SGD_logger4.log\
 		             --dim 28 28 1\
@@ -54,6 +58,7 @@ python3 train.py --optim Adam\
 		             --lr 0.01 --C 0.1 \
 		             --net CNN_4layers --bsize 512 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_Adam_model5.ckpt\
 		             --log ./running_log/MNIST_Adam_logger1.log\
 		             --dim 28 28 1\
@@ -66,6 +71,7 @@ python3 train.py --optim Adam\
 		             --lr 0.001 --C 0.1 \
 		             --net CNN_4layers --bsize 512 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_Adam_model6.ckpt\
 		             --log ./running_log/MNIST_Adam_logger2.log\
 		             --dim 28 28 1\
@@ -78,6 +84,7 @@ python3 train.py --optim Adam\
 		             --lr 0.0001 --C 0.1 \
 		             --net CNN_4layers --bsize 512 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_Adam_model7.ckpt\
 		             --log ./running_log/MNIST_Adam_logger3.log\
 		             --dim 28 28 1\
@@ -90,6 +97,7 @@ python3 train.py --optim Adam\
 		             --lr 0.00001 --C 0.1 \
 		             --net CNN_4layers --bsize 512 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_Adam_model8.ckpt\
 		             --log ./running_log/MNIST_Adam_logger4.log\
 		             --dim 28 28 1\
@@ -104,6 +112,7 @@ python3 train.py --optim NewtonCG\
 		             --net CNN_4layers --bsize 3000 \
 		             --GNsize 250 --CGmax  80 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_NewtonCG_model9.ckpt\
 		             --log ./running_log/MNIST_NewtonCG_logger1.log\
 		             --dim 28 28 1\
@@ -117,6 +126,7 @@ python3 train.py --optim NewtonCG\
 		             --net CNN_4layers --bsize 3000 \
 		             --GNsize 250 --CGmax  80 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_NewtonCG_model10.ckpt\
 		             --log ./running_log/MNIST_NewtonCG_logger2.log\
 		             --dim 28 28 1\
@@ -130,6 +140,7 @@ python3 train.py --optim NewtonCG\
 		             --net CNN_4layers --bsize 3000 \
 		             --GNsize 1000 --CGmax  80 \
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_NewtonCG_model11.ckpt\
 		             --log ./running_log/MNIST_NewtonCG_logger3.log\
 		             --dim 28 28 1\
@@ -143,6 +154,7 @@ python3 train.py --optim NewtonCG\
 		             --net CNN_4layers --bsize 3000 \
 		             --GNsize 1000 --CGmax  80\
 		             --train_set ./data/mnist-5000.mat \
+			     --val_set ./data/mnist.t.mat \
 		             --model ./saved_model/MNIST_NewtonCG_model12.ckpt\
 		             --log ./running_log/MNIST_NewtonCG_logger4.log\
 		             --dim 28 28 1\
