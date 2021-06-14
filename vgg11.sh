06d901f52ef1a96e383b670b4b3ae741bf4bfce0
Wan-Chen Chen <enlen111408@gmail.com>
2021-06-15 01:07:54 +0800

    Update vgg11.sh

--- vgg11.sh
+++ vgg11.sh
@@ -1,7 +1,7 @@
 python3 train.py --optim NewtonCG\
 		             --C 0.1 --lambda 0\
 		             --net VGG11 --bsize 3000 \
-		             --GNsize 250 --CGmax  80 \
+		             --GNsize 1000 --CGmax  80 \
 		             --train_set ./data/cifar10-5000.mat \
 			     --val_set ./data/cifar10.t.mat \
 		             --model ./saved_model/CIFAR10_NewtonCG_model11.ckpt \
