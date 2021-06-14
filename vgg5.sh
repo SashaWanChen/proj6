dc5269c9c0d0b2abfa89ff822c4bf474c658e392
Wan-Chen Chen <enlen111408@gmail.com>
2021-06-14 19:48:31 +0800

    Update vgg5.sh

--- vgg5.sh
+++ vgg5.sh
@@ -2,6 +2,7 @@ python3 train.py --optim Adam\
 		             --lr 0.01 --C 0.1 \
 		             --net VGG11 --bsize 512 \
 		             --train_set ./data/cifar10-5000.mat \
+			     --val_set ./data/cifar10.t.mat \
 		             --model ./saved_model/CIFAR10_Adam_model5.ckpt \
 		             --log ./running_log/CIFAR10_Adam_VGG_logger1.log\
 		             --seed 85
