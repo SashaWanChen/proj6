cfe125cc1a79659b28f9ae5b5eb80d06e5a1d52c
Wan-Chen Chen <enlen111408@gmail.com>
2021-06-14 19:48:28 +0800

    Update vgg4.sh

--- vgg4.sh
+++ vgg4.sh
@@ -2,6 +2,7 @@ python3 train.py --optim SGD\
 		             --lr 0.00001 --C 0.1 \
 		             --net VGG11 --bsize 512 \
 		             --train_set ./data/cifar10-5000.mat \
+			     --val_set ./data/cifar10.t.mat \
 		             --model ./saved_model/CIFAR10_SGD_model4.ckpt \
 		             --log ./running_log/CIFAR10_SGD_VGG_logger4.log\
 		             --seed 85
